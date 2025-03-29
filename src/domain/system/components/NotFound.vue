c360b1e1189e7fd28b61da1e53b622b8d852ce48
Victor Purice <victor.purice@ulbsibiu.ro>
2025-03-29 12:19:30 +0200

    feat: not found page

---
+++ src/domain/system/components/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="flex flex-column align-items-center justify-content-center min-h-screen text-center"
+  >
+    <div class="text-5xl font-bold">404</div>
+    <div class="text-2xl mt-2">Page Not Found</div>
+    <p class="mt-2">The page you are looking for does not exist.</p>
+    <Button label="Go Home" class="mt-4" @click="goHome" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router"
+import Button from "primevue/button"
+
+const router = useRouter()
+
+const goHome = () => {
+  router.push("/")
+}
+</script>
